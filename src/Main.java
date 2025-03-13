91aee1b69f533f73cc45fb01ee4c7502bc4700cd
ugauga19892556@gmail.com <ugauga19892556@gmail.com>
2025-03-13 14:03:10 +0900

    AtCoder rank B

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        int[] A = new int[N];
+        String[] strings = new String[N];
 
         for (int i = 0; i < N; i++) {
-            A[i] = scanner.nextInt();
+            strings[i] = scanner.next();
         }
 
-        if (isStrictlyIncreasing(A)) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
-        }
-    }
+        Arrays.sort(strings, Comparator.comparingInt(String::length));
 
-    public static boolean isStrictlyIncreasing(int[] A) {
-        for (int i = 0; i < A.length - 1; i++) {
-            if (A[i] >= A[i + 1]) {
-                return false;
-            }
+        StringBuilder result = new StringBuilder();
+        for (String s : strings) {
+            result.append(s);
         }
-        return true;
+
+        System.out.println(result.toString());
+
+        scanner.close();
     }
 }
