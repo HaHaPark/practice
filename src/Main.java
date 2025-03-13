a165c82b89aa7489b8cbfe3b42ab52e046084615
ugauga19892556@gmail.com <ugauga19892556@gmail.com>
2025-03-13 13:52:30 +0900

    AtCoder A

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = scanner.nextInt();
+        }
+
+        if (isStrictlyIncreasing(A)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+
+    public static boolean isStrictlyIncreasing(int[] A) {
+        for (int i = 0; i < A.length - 1; i++) {
+            if (A[i] >= A[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
