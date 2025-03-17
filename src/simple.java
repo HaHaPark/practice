0b4e62b0029be2786ae7601f9abe9c9657f74d8a
ugauga19892556@gmail.com <ugauga19892556@gmail.com>
2025-03-17 14:57:19 +0900

    simple

---
+++ src/simple.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.HashSet;
+
+public class simple {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+
+        int removals = 0;
+
+        HashSet<Long> seenEdges = new HashSet<>();
+
+        for (int i = 0; i < M; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            if (u == v) {
+                removals++;
+                continue;
+            }
+            int a = Math.min(u, v);
+            int b = Math.max(u, v);
+
+            //二つの頂点を一つの値に合わせる
+            long edgeId = (long) a * (N + 1) + b;
+
+            if (seenEdges.contains(edgeId)) {
+                removals++;
+            } else {
+
+                seenEdges.add(edgeId);
+            }
+        }
+
+        System.out.println(removals);
+    }
+}
